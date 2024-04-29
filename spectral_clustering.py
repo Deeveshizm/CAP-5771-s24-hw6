0504b2f9352f9ca269f8efc1c356c40514f9d7f6
Deeveshizm <deevesh.jain99@gmail.com>
2024-04-29 18:11:36 -0400

    test-3-spec-clust

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -304,13 +304,13 @@ def spectral_clustering():
 
     # Plot of the eigenvalues (smallest to largest) as a line plot.
     # Use the plt.plot() function. Make sure to include a title, axis labels, and a grid.
-    plot_eig, ax_eig = plt.subplots()
+    fig_eig, ax_eig = plt.subplots()
     ax_eig.plot(np.sort(eigenvalues), marker='o', linestyle='-')
-    ax_eig.set_title("Sorted Eigenvalues Plot")
+    plot_eig = ax_eig.set_title("Sorted Eigenvalues Plot")
     ax_eig.set_xlabel("Index")
     ax_eig.set_ylabel("Eigenvalue")
     ax_eig.grid(True)
-    
+    plt.close(fig_eig)
     answers["eigenvalue plot"] = plot_eig
 
 
